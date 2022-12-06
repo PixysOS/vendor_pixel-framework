2ffdb82cf65dcba1577530c52660266a71520401
Anay Wadhera <anay1018@gmail.com>
2022-12-07 01:24:19 +0900

    pixel-fw: Fix HeadsUpManagerPhone constructor
    Change-Id: I510579cdbc501c77b48e552be2bab51fe15c9221

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -64,6 +64,7 @@ import com.android.systemui.statusbar.phone.DozeServiceHost;
 import com.android.systemui.statusbar.phone.HeadsUpManagerPhone;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
+import com.android.systemui.statusbar.policy.AccessibilityManagerWrapper;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BatteryControllerImpl;
 import com.android.systemui.statusbar.policy.ConfigurationController;
@@ -172,7 +173,10 @@ public abstract class SystemUIGoogleModule {
             KeyguardBypassController bypassController,
             GroupMembershipManager groupManager,
             VisualStabilityProvider visualStabilityProvider,
-            ConfigurationController configurationController) {
+            ConfigurationController configurationController,
+            @Main Handler handler,
+            AccessibilityManagerWrapper accessibilityManagerWrapper,
+            UiEventLogger uiEventLogger) {
         return new HeadsUpManagerPhone(
                 context,
                 headsUpManagerLogger,
@@ -180,7 +184,10 @@ public abstract class SystemUIGoogleModule {
                 bypassController,
                 groupManager,
                 visualStabilityProvider,
-                configurationController
+                configurationController,
+                handler,
+                accessibilityManagerWrapper,
+                uiEventLogger
         );
     }
 
