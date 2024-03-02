18f6c3407e22cf887e538d7c9f633dbd2c98a794
someone5678 <someone5678@users.noreply.github.com>
2024-03-02 16:31:07 +0900

    pixel-framework: dex preopt configs for PixelFW
    Change-Id: I728b68e4996422faf5a0f7af042f3c0a61623ccf
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config.mk
+++ config.mk
@@ -2,4 +2,9 @@ PRODUCT_PACKAGES += \
     SystemUIGoogle \
     SettingsGoogle
 
+# Dex preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SettingsGoogle \
+    SystemUIGoogle
+
 $(call inherit-product, vendor/pixel-framework/common/common-vendor.mk)
